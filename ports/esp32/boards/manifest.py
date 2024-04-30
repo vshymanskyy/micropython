7360ce8170a49f3a6602718c8479230d1a021b4b
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2024-04-30 22:21:34 +0100

    Cleanup

--- ports/esp32/boards/manifest.py
+++ ports/esp32/boards/manifest.py
@@ -4,6 +4,7 @@ include("$(MPY_DIR)/extmod/asyncio")
 # Useful networking-related packages.
 require("mip")
 require("requests")
+require("ssl")
 
 # Require some micropython-lib modules.
 require("logging")
