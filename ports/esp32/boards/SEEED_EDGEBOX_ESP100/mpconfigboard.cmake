c37965fa00b5b847149765b0ba6451f8b712c83f
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2024-05-01 12:40:39 +0100

    Cleanup

--- ports/esp32/boards/SEEED_EDGEBOX_ESP100/mpconfigboard.cmake
+++ ports/esp32/boards/SEEED_EDGEBOX_ESP100/mpconfigboard.cmake
@@ -2,7 +2,6 @@ set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
-    ${SDKCONFIG_IDF_VERSION_SPECIFIC}
     boards/sdkconfig.ble
     boards/sdkconfig.spiram_sx
     boards/sdkconfig.240mhz
