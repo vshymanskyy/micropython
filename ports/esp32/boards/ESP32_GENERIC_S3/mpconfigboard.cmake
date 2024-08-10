ba49efe6bd7a8d8dfa907f81f67aec9af40df333
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2024-08-10 07:56:20 +0100

    Enable camera for ESP32-S3

--- ports/esp32/boards/ESP32_GENERIC_S3/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_GENERIC_S3/mpconfigboard.cmake
@@ -6,5 +6,10 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.usb
     boards/sdkconfig.ble
     boards/sdkconfig.spiram_sx
+    boards/sdkconfig.esp32cam
     boards/ESP32_GENERIC_S3/sdkconfig.board
+)
+
+list(APPEND MICROPY_DEF_BOARD
+    MODULE_CAMERA_ENABLED=1
 )
