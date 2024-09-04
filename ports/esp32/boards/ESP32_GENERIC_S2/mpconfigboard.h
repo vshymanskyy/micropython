6f39b8764404ea16332dff5368755918b440bef5
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2024-09-04 12:58:58 +0100

    Cleanup

--- ports/esp32/boards/ESP32_GENERIC_S2/mpconfigboard.h
+++ ports/esp32/boards/ESP32_GENERIC_S2/mpconfigboard.h
@@ -6,4 +6,3 @@
 
 // Enable UART REPL for modules that have an external USB-UART and don't use native USB.
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
-#define MICROPY_HW_ENABLE_CDC_REPL          (1)
